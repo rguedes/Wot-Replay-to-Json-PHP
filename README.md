34f8bef15403509b004ecaa35d06a3dda5fbcc21
rolando guedes <rolandoguedes@gmail.com>
2014-02-13 20:50:24 +0000

    Update to WoT 0.8.11 struct.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Extract JSON data from Replays
 Supported Versions
 ==================
 WoT 0.7.x and higher.
-Latest tested version: WoT 0.8.10.
+Latest tested version: WoT 0.8.11.
 
 Usage
 ==================
@@ -19,17 +19,15 @@ $wot->parse();
 
 Usage
 ==================
-ommon[status] = "ok" or "error"
+common[status] = "ok" or "error"
 common[message] = detailed error, otherwise "ok"
 
 common[datablock_1] = Datablock 1 exists
-common[datablock_2] = Datablock 2 exists
 common[datablock_battle_result] = Battle Result exists
 
 If the replay can be read, the file will contain additional blocks:
 
 datablock_1
-datablock_2 - not always available
 datablock_battle_result - available only for replays created by WoT 0.8.2 or higher Value of -1 is indicating a corrupt/wrong inserted Battle Result due to a bug in WoT
 
 Credits
--- parseWot.php
+++ parseWot.php
@@ -9,6 +9,9 @@ class parseWot {
 	}
 
 	public function parse(){
+		$replay_version = "0.0.0.0";
+		$replay_version_dict = array('0', '0', '0', '0');
+
 		$result_blocks = array();
 		$result_blocks['common'] = array();
 		$result_blocks['identify'] = array();
@@ -16,6 +19,7 @@ class parseWot {
 		$filename_source = $this->source;
 		$fp = fopen($filename_source, 'rb');
 		try{
+			//read content: [4:8]
 			fseek($fp, 4);
 			$contents = fread($fp, 4);
 			$contents = unpack("I", $contents);
@@ -29,6 +33,7 @@ class parseWot {
 			$result_blocks['common']['message'] = $e->getMessage();
 			return $result_blocks;
 		}
+
 		if ($numofblocks == 0) {
 			$result_blocks['common']['message'] = "unknown file structure";
 			return $result_blocks;
@@ -54,7 +59,13 @@ class parseWot {
 					$myblock = fread($fp, $value);
 
 					if (strpos($myblock,'arenaUniqueID') !== false) {
-						$br_block = phpickle::loads($myblock);
+
+						if( ($replay_version_dict[1] == 8 and $replay_version_dict[2] > 10) or $replay_version_dict[1] > 8 or $myblock[0]=='[' ){
+							$br_json_list = json_decode($myblock, true);
+							$br_block = $br_json_list[0];
+						}else{
+							$br_block = phpickle::loads($myblock);
+						}
 						foreach ($br_block['vehicles'] as $key => $value) {
 							if(array_key_exists("details", $value)){
 								unset($br_block['vehicles'][$key]['details']);
@@ -63,10 +74,17 @@ class parseWot {
 						$result_blocks['datablock_battle_result'] = $br_block;
 
 						$result_blocks['common']['datablock_battle_result'] = 1;
-						$result_blocks['identify']['arenaUniqueID'] = $result_blocks['datablock_battle_result']['arenaUniqueID'];
+						$result_blocks['identify']['arenaUniqueID'] = strval($result_blocks['datablock_battle_result']['arenaUniqueID']);
 
 					}else{
+
 						$blockdict = json_decode($myblock, true);
+						if (array_key_exists("clientVersionFromExe", $blockdict)) {
+							$replay_version = $this->cleanReplayVersion($blockdict['clientVersionFromExe']);
+							$result_blocks['common']['replay_version'] = $replay_version;
+							$result_blocks['identify']['replay_version'] = $replay_version;
+							$replay_version_dict = explode('.', $replay_version);
+						}
 						$result_blocks['datablock_' . $i] = $blockdict;
 						$result_blocks['common']['datablock_' . $i] = 1;
 					}
@@ -77,10 +95,18 @@ class parseWot {
 			}
 
 		}
+
+		$result_blocks = $this->get_identify($result_blocks);
 		fclose($fp);
 		$this->dumpjson($result_blocks, $filename_source,0);
 		return $result_blocks;
 	}
+
+	function cleanReplayVersion($replay_version){
+			$replay_version = str_replace(array(", ",' '), array(".",'.'), $replay_version);
+			return $replay_version;
+	}
+
 	function dumpjson($mydict, $filename_source, $exitcode){
 		if ($exitcode == 0) {
 			$mydict['common']['status'] = "ok";
@@ -94,5 +120,57 @@ class parseWot {
 		fwrite($finalfile, json_encode($mydict));
 		fclose($finalfile);
 	}
+
+	function get_identify($result_blocks){
+
+
+
+		$team = 0;
+		$internaluserID = 0;
+
+		foreach ($result_blocks['datablock_1']['vehicles'] as $key => $value) {
+			if ( $result_blocks['datablock_1']['vehicles'][$key]['name'] == $result_blocks['datablock_1']['playerName'] ){
+				$internaluserID = $key;
+				$team = $value['team'];
+				break;
+			}
+		}
+		if($team>0){
+			if($result_blocks['datablock_battle_result']['common']['winnerTeam'] == $team)
+				$result_blocks['identify']['isWinner'] = 1;
+			elseif($result_blocks['datablock_battle_result']['common']['winnerTeam'] == "")
+				$result_blocks['identify']['isWinner'] = -1;
+			else
+				$result_blocks['identify']['isWinner'] = 0;
+		}
+
+		$result_blocks['identify']['internaluserID'] = $internaluserID;
+		$result_blocks['identify']['arenaCreateTime'] = $result_blocks['datablock_1']['dateTime'];
+		$result_blocks['identify']['playername'] = $result_blocks['datablock_1']['playerName'];
+		$result_blocks['identify']['accountDBID'] = $result_blocks['datablock_1']['playerID'];
+		$result_blocks['identify']['mapName'] = $result_blocks['datablock_1']['mapName'];
+
+		$result_blocks['identify']['error'] = 'none';
+		$result_blocks['identify']['error_details'] = 'none';
+
+
+		if (!array_key_exists("datablock_battle_result", $result_blocks['common']) )
+			return $result_blocks;
+
+		foreach($result_blocks['datablock_battle_result']['players'] as $key => $value) {
+			$result_blocks['datablock_battle_result']['players'][$key]['platoonID'] = $result_blocks['datablock_battle_result']['players'][$key]['prebattleID'];
+			foreach ($result_blocks['datablock_battle_result']['vehicles'] as $vkey => $vvalue) {
+				if ( $result_blocks['datablock_battle_result']['vehicles'][$vkey]['accountDBID'] == $key ){
+		            $result_blocks['datablock_battle_result']['players'][$key]['vehicleid'] = $vkey;
+		            break;
+		        }
+			}
+		}
+
+		return $result_blocks;
+
+	}
 }
+
+
 ?>
